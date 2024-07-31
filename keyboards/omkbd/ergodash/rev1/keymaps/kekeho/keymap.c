81f31e655de4a044408e56dee3c2bd3943212620
kekeho@sfc.wide.ad.jp <hirodora@me.com>
2024-08-01 02:05:10 +0900

    add : change lang code

--- keyboards/omkbd/ergodash/rev1/keymaps/kekeho/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/kekeho/keymap.c
@@ -1,12 +1,46 @@
 #include QMK_KEYBOARD_H
 
+
+enum custom_keycodes {
+    LANG_TOGGLE = SAFE_RANGE,
+};
+
+
+static bool lng = true;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LANG_TOGGLE:
+            if (lng) {
+              if (record->event.pressed) {
+                  // Ctrl + Space を送信
+                  register_code(KC_LNG1);
+              } else {
+                  unregister_code(KC_LNG1);
+                  lng = !lng;
+              }
+            } else {
+              if (record->event.pressed) {
+                  // Ctrl + Space を送信
+                  register_code(KC_LNG2);
+              } else {
+                  unregister_code(KC_LNG2);
+                  lng = !lng;
+              }
+            }
+            return false;
+    }
+    return true;
+}
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
     KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_ESC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LALT, KC_SCLN, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
     KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_BSPC, KC_QUOT, KC_H, KC_J, KC_K, KC_L, KC_LBRC, KC_RBRC,
-    KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(2), MO(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RGUI(RSFT(KC_P)),
-    KC_NO, KC_UP, KC_DOWN, KC_LALT, KC_LCTL, KC_SPC, KC_RGUI, KC_ENT, KC_RSFT, RCTL(KC_SPC), KC_RALT, KC_LEFT, KC_RGHT, HYPR(KC_NO)
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(2), MO(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RGUI(RSFT(KC_P)),
+    KC_NO, KC_UP, KC_DOWN, KC_LALT, KC_LCTL, KC_SPC, KC_RGUI, KC_ENT, KC_RSFT, LANG_TOGGLE, KC_RALT, KC_LEFT, KC_RGHT, HYPR(KC_NO)
   ),
 	
 	[1] = LAYOUT(
@@ -22,6 +56,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LALT, KC_SCLN, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
     KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, KC_BSPC, KC_QUOT, KC_H, KC_J, KC_K, KC_L, KC_LBRC, KC_RBRC,
     KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(2), MO(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RGUI(RSFT(KC_P)),
-    KC_NO, KC_UP, KC_DOWN, KC_LALT, KC_LCTL, KC_SPC, KC_RGUI, KC_ENT, KC_RSFT, RCTL(KC_SPC), KC_RALT, KC_LEFT, KC_RGHT, HYPR(KC_NO)
+    KC_NO, KC_UP, KC_DOWN, KC_LALT, KC_LCTL, KC_SPC, KC_RGUI, KC_ENT, KC_RSFT, LANG_TOGGLE, KC_RALT, KC_LEFT, KC_RGHT, HYPR(KC_NO)
   )
 };
